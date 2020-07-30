c0e718c41fa6e6cd3e5be01a69c84a57dc85103e
Ariel Chura <lf0ariel@gmail.com>
2020-07-30 03:05:07 -0400

    first example hook

---
+++ src/components/01-useState/CounterApp.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+export const CounterApp = () => {
+
+  const [state, setstate] = useState({
+    valor1: 10,
+    valor2: 20,
+    valor3: 30,
+  })
+
+  const { valor1, valor2 } = state;
+
+  return (
+    <>
+      <h1>valor 1 { valor1 }</h1>
+      <h1>valor 1 { valor2 }</h1>
+      <hr/>
+
+      <button className="border-2 hover:bg-teal-100  w-24 rounded hover:border-teal-600 focus:outline-none"
+        onClick={ () => {
+          setstate({
+            ...state,
+            valor1 : valor1+1
+          })
+        }}
+      >+1</button>
+    </>
+  )
+}
