91cbd591ceae2286c9c4398c6c1a75212b5ee1ef
Ariel Chura <lf0ariel@gmail.com>
2020-08-06 23:23:37 -0400

    useMemo

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 ## Use Effect
 
 El use efect es un hook que permite ejecutar algun efecto secundario cuando ocurre algun cambio en algun componente
+
+## Funcion Memo
+
+Es una funcion que graba en memoria un componente y la ejecuta desde la memoria mentras este no sufra cambios en sus propiedades.
---
+++ src/components/06-memos/MemoHook.js
@@ -0,0 +1,37 @@
+import React, { useState, useMemo } from 'react'
+import { useCounter } from '../../hooks/useCounter'
+
+export const MemoHook = () => {
+  const { counter, increment } = useCounter(5000);
+  const [toggle, setToggle] = useState(true)
+
+  const RecursoGrande = iterations => {
+    for (let i = 0; i < iterations; i++) {
+      console.log(`se ejecutaron: ${iterations} veces` );
+    }
+    return `se ejecutaron: ${iterations} veces`
+  }
+
+//Use Memo
+
+const memoRecursoGrande = useMemo(() => RecursoGrande( counter ), [ counter ])
+
+
+  return (
+    <div className="w-4/5 m-auto mt-20 text-gray-800">
+      <h1 className="text-3xl text-center font-bold">Use Memo Hook : { counter }</h1>
+      <hr/>
+      <p>{ memoRecursoGrande }</p>
+      <button
+        className="rounded-lg bg-indigo-500 text-white font-semibold m-2 py-1 px-6"
+        onClick={ increment }
+      >+1</button>
+      <button
+        className="rounded-lg bg-teal-500 text-white font-semibold m2-2 py-1 px-6"
+        onClick={ () => setToggle( !toggle ) }
+      > show/hide { JSON.stringify( toggle ) }
+      </button>
+    </div>
+  )
+
+}
